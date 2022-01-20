e96d104c71824c972819743cbf1496dc61a386af
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2022-01-19 18:51:52 -0600

    se agrego swagger de modulo role

--- src/modules/role/dto/create.role.dto.ts
+++ src/modules/role/dto/create.role.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength } from 'class-validator';
 
 export class CreateRoleDto {
   @IsString()
+  @ApiProperty({ example: 'USER9' })
   @MaxLength(50, { message: 'The name is not valid' })
   readonly name: string;
 
   @IsString()
+  @ApiProperty({ example: 'este es un usuario con nivel 9' })
   @MaxLength(100, { message: 'this description not valid' })
   readonly description: string;
 
